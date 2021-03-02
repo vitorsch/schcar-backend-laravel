2063be6aa7d4d2c977dbfb16e754e43779fa830a
vitorsch <vitorsch@gmail.com>
2021-03-02 19:58:22 -0300

    Controlador do Veículo e rotas ( pequena mudança no RouteServiceProvider.php para corrigir erro de namespace)

--- app/Http/Controllers/api/VehiclesController.php
+++ app/Http/Controllers/api/VehiclesController.php
@@ -3,60 +3,81 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Vehicle;
+use App\Models\Vehicle_car_steering;
+use App\Models\Vehicle_carcolor;
+use App\Models\Vehicle_cubiccms;
+use App\Models\Vehicle_doors;
+use App\Models\Vehicle_exchange;
+use App\Models\Vehicle_features;
+use App\Models\Vehicle_financial;
+use App\Models\Vehicle_fuel;
+use App\Models\Vehicle_gearbox;
+use App\Models\Vehicle_motorpower;
+use App\Models\Vehicle_regdate;
+use App\Models\Vehicle_type;
 use Illuminate\Http\Request;
 
 class VehiclesController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+    protected $user;
+
+    public function __construct()
+    {
+        $this->user = Auth()->guard('api')->user();
+        // $this->middleware(function ($request, $next) {
+        //     $this->user = Auth::user();
+        //     return $next($request);
+        // }); 
+    }
+
+    private function getData() 
+    {
+        return [
+            'vehicle_types' => Vehicle_type::all(),
+            'regdate' => Vehicle_regdate::orderBy('label', 'ASC')->get(),
+            'gearbox' => Vehicle_gearbox::all(),
+            'fuel' => Vehicle_fuel::all(),
+            'car_steering' => Vehicle_car_steering::all(),
+            'motorpower' => Vehicle_motorpower::all(),
+            'doors' => Vehicle_doors::all(),
+            'features' => Vehicle_features::all(),
+            'carcolor' => Vehicle_carcolor::all(),
+            'exchange' => Vehicle_exchange::all(),
+            'financial' => Vehicle_financial::all(),
+            'cubiccms' => Vehicle_cubiccms::all()
+            
+        ];
+    }
+    
     public function index()
     {
         //
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
+    
     public function store(Request $request)
     {
-        //
+        $vehicle = Vehicle::with('vehicle_photos')
+            ->firstOrCreate([
+                'user_id' => $this->user->id,
+                'status' => 0
+            ]);
+
+        return array_merge(['vehicle' => $vehicle], $this->getData());
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
         //
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
+    
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Vehicle extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id'
+    ];
+    
+    public function vehicle_photos() {
+        return $this->hasMany('App\Models\Vehicle_photos', 'vehicle_id', 'id')
+            ->orderBy('order', 'ASC');
+    }
+
 }
